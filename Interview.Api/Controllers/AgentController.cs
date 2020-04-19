ca2f1121b66e48259ada69b4fcfe0797fec214b0
Nove developer <nove1398@hotmail.com>
2020-04-19 02:10:01 -0500

    Add project files.

---
+++ Interview.Api/Controllers/AgentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Interview.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentController : ControllerBase
+    {
+        private ILogger<AgentController> _ilog;
+
+        public AgentController(ILogger<AgentController> ilogger)
+        {
+            _ilog = ilogger;
+        }
+
+        public void Get()
+        {
+
+        }
+    }
+}
